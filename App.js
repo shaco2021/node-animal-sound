feee19d3dd2fc4f0f6ac15017d17776edc16439b
shaco2021 <157100090+shaco2021@users.noreply.github.com>
2024-01-29 11:50:06 +0900

    FIRST COMMIT

---
+++ App.js
@@ -0,0 +1,23 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'gray',
+  },
+
+  item1: {
+    width: '50%',
+    height: 100,
+    backgroundColor: 'blue',
+  },
+  item2: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  item3: {
+    flex: 2,
+    height: 100,
+    backgroundColor: 'orange',
+  },
+});
